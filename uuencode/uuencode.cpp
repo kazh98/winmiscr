648d144f6651304ad384b7c6e0490cda3ff3facc
Kazuhiro HISHINUMA <kaz@cs.meiji.ac.jp>
2019-07-11 22:44:24 +0900

    プロジェクト ファイルを追加します。

---
+++ uuencode/uuencode.cpp
@@ -0,0 +1,14 @@
+﻿#include <cstdio>
+#include <cstdlib>
+#include <cwchar>
+
+
+static const wchar_t ttable[] = L" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_";
+
+
+int wmain(int argc, wchar_t *argv[], wchar_t* envp[])
+{
+    wprintf(L"Hello World!\n");
+
+	return EXIT_SUCCESS;
+}
